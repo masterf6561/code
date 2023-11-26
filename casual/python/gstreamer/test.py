4fd26b2ad4faf4681987a415e298828b502a104d
masterf6561 <moritz.lechner02@gmail.com>
2023-11-26 19:49:54 +0100

    [add] gstreamer

--- casual/python/gstreamer/test.py
+++ casual/python/gstreamer/test.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import sys
 import time
 
@@ -10,13 +8,13 @@ from gi.repository import Gst # type: ignore
 def main(argv: list[str]):
     Gst.init(argv)
 
-    pipeline = Gst.Pipeline.new("mypipeline")
+    pipeline = Gst.parse_launch("playbin uri=sample-9s.mp3")
 
-    src = Gst.ElementFactory.make("videotestsrc")
-    sink = Gst.ElementFactory.make("autovideosink")
+    src = Gst.ElementFactory.make("videotestsrc", "video-source")
+    sink = Gst.ElementFactory.make("autoaudiosink", "audio-sink")
 
     src.set_property("pattern", "ball")
-    src.set_property("background-color", 0x00FF0000)
+    src.set_property("background-color", 0xFFFF0000)
 
     pipeline.add(src)
     pipeline.add(sink)
@@ -24,8 +22,9 @@ def main(argv: list[str]):
     src.link(sink)
 
     pipeline.set_state(Gst.State.PLAYING)
-
+    
     time.sleep(5)
+    print("Pipeline done")
 
     pipeline.set_state(Gst.State.NULL)
 
